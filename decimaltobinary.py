4d9e64f0fc6e31c5fffc4be38365bcdb0c7d3730
Amaris Efthimiou <51919178+amaris-eft@users.noreply.github.com>
2021-03-22 00:01:43 -0400

    Add files via upload

---
+++ decimaltobinary.py
@@ -0,0 +1,9 @@
+#Amaris Efthimiou
+#November 2019
+#37 Converter
+
+binString = input("Enter a binary number: ")
+decNum = 0
+for c in binString:
+    decNum = decNum*2 + c(decNum)
+print("Your number in decimal is", decNum)
